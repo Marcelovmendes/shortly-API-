9181804ab17d12f0933a6630be0a73c2a9ad4dc6
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-05-22 14:08:06 -0300

    fix: validatesign

--- src/controllers/sign.controller.js
+++ src/controllers/sign.controller.js
@@ -20,9 +20,8 @@ export async function signIn(req, res) {
     const {user} = res.locals
     try{
     const token = jwt.sign({userId:user.id}, process.env.JWT_SECRET)
-   console.log(token)
     await db.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2)`, [user.id, token])
-
+ console.log()
     res.status(200).send({token})
     }catch(err){
         console.log(err)
--- src/middlewares/validateSign.middleware.js
+++ src/middlewares/validateSign.middleware.js
@@ -12,8 +12,9 @@ next()
 
 export async function validateSign (req,res,next){
  const {email,password} = req.body
-
+ 
   const user = await db.query('SELECT * FROM users WHERE email = $1', [email])
+  console.log(user)
   if(!user || user.rows.length===0) return res.status(401).send({ message: "Unauthorized" })
   const passwordMatch = bycript.compareSync(password, user.rows[0].password)
   if(!passwordMatch) return res.status(401).send({ message: "Unauthorized" })

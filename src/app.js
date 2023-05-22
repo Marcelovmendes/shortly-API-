083b333ac3fdf969ba71f49a4e904681651026fd
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-05-22 12:04:21 -0300

    fix: database.connection

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import shortenRouter from "./routes/shorten.routes.js";
+import dotenv from "dotenv";
 
 
 
@@ -11,7 +12,8 @@ app.use(cors())
 app.use(express.json())
 app.use(authRouter)
 app.use(shortenRouter)
+dotenv.config()
 
 
-const PORT = 5000
+const PORT = process.env.PORT ||  5000
 app.listen(PORT, ()=> console.log("Server is running on port " + PORT))

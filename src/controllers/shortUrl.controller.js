7b494e7df6e18a0f0ab2ec16a59fe4fd064fb612
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-05-21 23:54:27 -0300

    feat:deleteUrl,getUser,getranking without test

--- src/controllers/shortUrl.controller.js
+++ src/controllers/shortUrl.controller.js
@@ -1,13 +1,14 @@
-import {db} from "../database/database.connection.js";
+import { func } from "joi";
+import { db } from "../database/database.connection.js";
 import { nanoid } from "nanoid";
-export  async function postShorten(req, res) {
+export async function postShorten(req, res) {
   const { url } = req.body;
   const { session } = res.locals;
-  req.body= nanoid(6)
-  const shortUrl = req.body
+  req.body = nanoid(6);
+  const shortUrl = req.body;
   try {
     const userId = session;
-    
+
     await db.query(
       `INSERT INTO urls ("originalUrl", "shortCode", "userId") VALUES ($1, $2, $3)`,
       [url, shortUrl, userId]
@@ -16,44 +17,125 @@ export  async function postShorten(req, res) {
       `SELECT urls.id, urls."shortCode" FROM urls WHERE "shortCode"=$1 AND "userId"=$2`,
       [shortUrl, userId]
     );
-    
+
     res.status(201).send(body.rows[0]);
   } catch (err) {
     console.log(err);
     res.status(500).send(err.message);
   }
 }
-export async function getUrlById(req,res){
-  const {id}= req.params;
+export async function getUrlById(req, res) {
+  const { id } = req.params;
 
- try{
-   const body = await db.query(`SELECT * FROM urls WHERE id=$1`,[id]);
-   if(!body.rows.length ===0) return res.status(404).send({ message: "Not Found" });
+  try {
+    const body = await db.query(`SELECT * FROM urls WHERE id=$1`, [id]);
+    if (!body.rows.length === 0)
+      return res.status(404).send({ message: "Not Found" });
 
-   res.status(200).send(body.rows[0]);
-
- }catch(err){
-   console.log(err);
-   res.status(500).send(err.message);
- }
+    res.status(200).send(body.rows[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
 }
-export async function redirectUrl(req,res){
-  const {shortCode}= req.params;
+export async function redirectUrl(req, res) {
+  const { shortCode } = req.params;
 
-   try{
-     const result = await db.query(`SELECT * FROM urls WHERE "shortCode"=$1`,[shortCode]);
-     if (result.rows.length === 0) return res.status(404).send({ message: "Not Found" });
+  try {
+    const result = await db.query(`SELECT * FROM urls WHERE "shortCode"=$1`, [
+      shortCode,
+    ]);
+    if (result.rows.length === 0)
+      return res.status(404).send({ message: "Not Found" });
 
-     const url = result.rows[0].originalUrl;
-     const visitCount = result.rows[0].visitCount + 1;
- 
-     await db.query(
-       `UPDATE urls SET "shortCode" = $1 WHERE "visitCount" = $2`,
-       [shortCode, visitCount]
-     );
-res.redirect(url)
-   }catch(err){
-     console.log(err);
-   }
+    const url = result.rows[0].originalUrl;
+    const visitCount = result.rows[0].visitCount + 1;
 
+    await db.query(`UPDATE urls SET "shortCode" = $1 WHERE "visitCount" = $2`, [
+      shortCode,
+      visitCount,
+    ]);
+    res.redirect(url);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+}
+
+export async function deleteUrl(req, res) {
+  const { id } = req.params;
+  try {
+    const result = await db.query(`DELETE FROM urls WHERE id=$1`, [id]);
+
+    if (!result) return res.status(404).send({ message: "Not Found" });
+
+    res.sendStatus(204);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+}
+export async function getUser(req, res) {
+  try {
+    const { userId } = res.locals.session;
+
+    const result = await db.query(
+      `SELECT u.id, u.name,
+      (
+        SELECT COALESCE(SUM(url."visitCount"), 0)
+        FROM urls url
+        WHERE url."userId" = u.id) as visitCount,
+        JSON_AGG(JSON_BUILD_OBJECT(
+          'id', url.id,
+          'shortUrl', url."shortCode",
+          'url', url."originalUrl",
+          'visitCount', url."visitCount"
+        )) as shortenedUrls
+      FROM users u
+      LEFT JOIN urls url ON u.id = url."userId"
+      WHERE u.id = $1
+      GROUP BY u.id`,
+      [userId]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    const user = {
+      id: result.rows[0].id,
+      name: result.rows[0].name,
+      visitCount: parseInt(result.rows[0].visitCount) || 0,
+      shortenedUrls: result.rows[0].shortenedUrls || [],
+    };
+
+    res.status(200).send(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+}
+
+export async function getRanking(req, res) {
+  try {
+    const ranking = await db.query(
+      `SELECT u.id,u.name,(COUNT(url.id), as linksCount), (CoALESCE(SUM(url."visitCount"), 0))) 
+       FROM users u 
+       LEFT JOIN urls url ON u.id = url."userId" 
+       GROUP BY u.id 
+       ORDER BY visitCount DESC  ) `
+    );
+    const formattedRanking = ranking.rows.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        linksCount: user.linkscount,
+        visitCount: user.visitcount,
+      };
+    });
+
+    res.status(200).send(formattedRanking.rows);
+  } catch {
+    res.status(500).send(err.message);
+  }
 }
--- src/middlewares/authValidate.middleware.js
+++ src/middlewares/authValidate.middleware.js
@@ -7,9 +7,37 @@ export async function validateShorten(req,res,next){
     if(!token) return res.status(401).send({ message: "Unauthorized" })
 
     const session = await db.query('SELECT * FROM sessions WHERE token = $1', [token])
-    console.log("userIdV",session.rows[0].userId)
     if(session.rows.length===0) return res.status(401).send({ message: "Not Found" })
     
     res.locals.session = session.rows[0].userId
     next()
+}
+export async function validateGetUserShorten( req,res,next){
+  const {authorization} = req.headers
+  const {id} = req.params 
+  const token = authorization?.replace("Bearer ", "")
+  if(!token) return res.status(401).send({ message: "Unauthorized" })
+
+  const session = await db.query('SELECT * FROM sessions WHERE token = $1', [token])
+  if(session.rows.length===0) return res.status(401).send({ message: "Session Not Found" })
+
+  const userToken = await db.query(`SELECT url."userId" url."id" from urls WHERE "userId"=$1 AND  "id"=$2` , [userId], [id])
+
+  if( userToken.rows[0] === undefined) return res.status(401).send({ message: " unauthorized" })
+  res.locals.userId= userToken.rows[0].url.userId
+
+  next()
+}
+export async function getUser(req,res,next){
+   const {authorization} = req.headers
+   const token = authorization?.replace("Bearer ", "")
+   if(!token) return res.status(401).send({ message: "Unauthorized" })
+   const session = await db.query('SELECT * FROM sessions WHERE token = $1', [token])
+   if(session.rows.length===0) return res.status(401).send({ message: "Session Not Found" })
+   
+   const userId= session.rows[0].userId
+  res.locals.userId= userId
+   next()
+
+
 }

0a5ac5f057092f0e48a367a2f4ab89580bc38e6c
Marcelovmendes <marcelo.vitor@engenharia.ufjf.br>
2023-05-22 12:30:45 -0300

    fix : database connection without test deploy

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -8,6 +8,6 @@ const { Pool } = pg;
 const connection = {
   connectionString: process.env.DATABASE_URL,
 };
-if (process.env.MODE === "prod") configDatabase.ssl = true;
+if (process.env.MODE === "prod") connection.ssl = true;
 
 export const db = new Pool (connection);
